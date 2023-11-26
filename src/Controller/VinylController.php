a3b8608bf298594318cf5387c9597dc6c570025f
Francisdailin Cobas <francisdailin@gmail.com>
2023-11-27 00:18:48 +0100

    Files

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+
+class VinylController extends AbstractController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        $tracks = [
+            ['song' => 'Crhistmast', 'artist' => 'Mariah Carey']
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks
+        ]);
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse($slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Genre: ' . u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'All titles';
+        }
+        return new Response($title);
+    }
+}
